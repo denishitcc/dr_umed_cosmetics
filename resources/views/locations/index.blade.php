8ae4f188f7c61f489fe14f6776c8c497e820b837
denishitcc <102940538+denishitcc@users.noreply.github.com>
2023-12-12 11:32:01 +0530

    location page datatable changes

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -11,9 +11,10 @@
                 <h4 class="small-title mb-0">All Locations</h4>
             </div>
               <div class="card-body">
-                      <table id="example" class="table all-db-table align-middle" cellspacing="0" width="100%">
+                      <table id="example" class="table all-db-table align-middle display select" cellspacing="0" width="100%">
                         <thead>
                           <tr>
+                            <th><input type="checkbox" name="select_all" value="1" id="example-select-all"></th>
                             <th>Location Name</th>
                             <th>Email</th>
                             <th>Phone</th>
@@ -27,6 +28,7 @@
                           @if(count($locations)>0)
                           @foreach($locations as $loc)
                           <tr>
+                            <td></td>
                             <td><a href="#" class="blue-bold">{{$loc->location_name}}</a></td>
                             <td>{{$loc->email}}</td>
                             <td>{{$loc->phone}}</td>
@@ -34,7 +36,7 @@
                             <td>{{$loc->latitude}}</td>
                             <td>{{$loc->longitude}}</td>
                             <td>
-                              <div class="action-box"><button type="button" class="btn btn-sm black-btn round-6 far fa-edit">
+                              <div class="action-box"><button type="button" class="btn btn-sm black-btn round-6 far dt-edit">
                                   <i class="ico-edit"></i>
                                 </button>
                                 <button type="button" class="btn btn-sm black-btn round-6 dt-delete">
@@ -75,87 +77,81 @@
     @section('script')
     <script>
         $(document).ready(function() {
-          //Only needed for the filename of export files.
-          //Normally set in the title tag of your page.
-          document.title='Locations';
-          // DataTable initialisation
-         
-            $('#example').DataTable(
-            {
-              "dom": 'Blfrtip',
-              "paging": true,
-              "pageLength": 5,
-              "autoWidth": true,
-              "columnDefs": [
-                { "orderable": false, "targets": 5 }
-              ],
-              "buttons": [
-                'colvis',
-                'csvHtml5',
-                'excelHtml5',
-                'pdfHtml5',
-              ]
-            }
-          );
-          //Add row button
-          $('.dt-add').each(function () {
-            $(this).on('click', function(evt){
-              //Create some data and insert it
-              var rowData = [];
-              var table = $('#example').DataTable();
-              //Store next row number in array
-              var info = table.page.info();
-              rowData.push(info.recordsTotal+1);
-              //Some description
-              rowData.push('New Order');
-              //Random date
-              var date1 = new Date(2016,01,01);
-              var date2 = new Date(2018,12,31);
-              var rndDate = new Date(+date1 + Math.random() * (date2 - date1));//.toLocaleDateString();
-              rowData.push(rndDate.getFullYear()+'/'+(rndDate.getMonth()+1)+'/'+rndDate.getDate());
-              //Status column
-              rowData.push('NEW');
-              //Amount column
-              rowData.push(Math.floor(Math.random() * 2000) + 1);
-              //Inserting the buttons ???
-              rowData.push('<button type="button" class="btn btn-primary btn-xs dt-edit" style="margin-right:16px;"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button><button type="button" class="btn btn-danger btn-xs dt-delete"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>');
-              //Looping over columns is possible
-              //var colCount = table.columns()[0].length;
-              //for(var i=0; i < colCount; i++){      }
-              
-              //INSERT THE ROW
-              table.row.add(rowData).draw( false );
-            });
-          });
-          //Edit row buttons
-          $('.dt-edit').each(function () {
-            $(this).on('click', function(evt){
-              $this = $(this);
-              var dtRow = $this.parents('tr');
-              $('div.modal-body').innerHTML='';
-              $('div.modal-body').append('Row index: '+dtRow[0].rowIndex+'<br/>');
-              $('div.modal-body').append('Number of columns: '+dtRow[0].cells.length+'<br/>');
-              for(var i=0; i < dtRow[0].cells.length; i++){
-                $('div.modal-body').append('Cell (column, row) '+dtRow[0].cells[i]._DT_CellIndex.column+', '+dtRow[0].cells[i]._DT_CellIndex.row+' => innerHTML : '+dtRow[0].cells[i].innerHTML+'<br/>');
+            document.title='Locations';
+            var table = $('#example').DataTable({
+            "dom": 'Blfrtip',
+            "paging": true,
+            "pageLength": 5,
+            "autoWidth": true,
+            'columnDefs': [{
+              'targets': 0,
+              'searchable': false,
+              'orderable': false,
+              'className': 'dt-body-center',
+              'render': function (data, type, full, meta){
+                  return '<input type="checkbox" name="id[]" value="' + $('<div/>').text(data).html() + '">';
               }
-              $('#myModal').modal('show');
-            });
-          });
-          //Delete buttons
-          $('.dt-delete').each(function () {
-            $(this).on('click', function(evt){
-              $this = $(this);
-              var dtRow = $this.parents('tr');
-              if(confirm("Are you sure to delete this row?")){
-                var table = $('#example').DataTable();
-                table.row(dtRow[0].rowIndex-1).remove().draw( false );
-              }
-            });
-          });
-          $('#myModal').on('hidden.bs.modal', function (evt) {
-            $('.modal .modal-body').empty();
-          });
-        });
+            }],
+            "buttons": [
+              'colvis',
+              'csvHtml5',
+              'excelHtml5',
+              'pdfHtml5',
+            ],
+            'order': [[1, 'asc']]
+    });
+
+    // Handle click on "Select all" control
+    $('#example-select-all').on('click', function(){
+        // Get all rows with search applied
+        var rows = table.rows({ 'search': 'applied' }).nodes();
+        // Check/uncheck checkboxes for all rows in the table
+        $('input[type="checkbox"]', rows).prop('checked', this.checked);
+    });
+
+    // Handle click on checkbox to set state of "Select all" control
+    $('#example tbody').on('change', 'input[type="checkbox"]', function(){
+        // If checkbox is not checked
+        if(!this.checked){
+          var el = $('#example-select-all').get(0);
+          // If "Select all" control is checked and has 'indeterminate' property
+          if(el && el.checked && ('indeterminate' in el)){
+              // Set visual state of "Select all" control
+              // as 'indeterminate'
+              el.indeterminate = true;
+          }
+        }
+    });
+    //Edit row buttons
+    $('.dt-edit').each(function () {debugger;
+      $(this).on('click', function(evt){
+        $this = $(this);
+        var dtRow = $this.parents('tr');
+        $('div.modal-body').innerHTML='';
+        $('div.modal-body').append('Row index: '+dtRow[0].rowIndex+'<br/>');
+        $('div.modal-body').append('Number of columns: '+dtRow[0].cells.length+'<br/>');
+        for(var i=0; i < dtRow[0].cells.length; i++){
+          $('div.modal-body').append('Cell (column, row) '+dtRow[0].cells[i]._DT_CellIndex.column+', '+dtRow[0].cells[i]._DT_CellIndex.row+' => innerHTML : '+dtRow[0].cells[i].innerHTML+'<br/>');
+        }
+        $('#myModal').modal('show');
+      });
+    });
+    //Delete buttons
+    $('.dt-delete').each(function () {
+      $(this).on('click', function(evt){
+        $this = $(this);
+        var dtRow = $this.parents('tr');
+        if(confirm("Are you sure to delete this row?")){
+          var table = $('#example').DataTable();
+          table.row(dtRow[0].rowIndex-1).remove().draw( false );
+        }
+      });
+    });
+    $('#myModal').on('hidden.bs.modal', function (evt) {
+      $('.modal .modal-body').empty();
+    });
+
+  });
     </script>
 </body>
 
