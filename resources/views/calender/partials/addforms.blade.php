c6234bb6d2482aaa4fa8daf77fcc12428bc4ba46
Dharit Maniyar <dharit@devsinindia.com>
2024-05-21 16:43:07 +0530

    bug fixes for appointment id

--- resources/views/calender/partials/addforms.blade.php
+++ resources/views/calender/partials/addforms.blade.php
@@ -2,9 +2,8 @@
     @foreach ($forms as $form)
         <li class="list-group-item">
             <label class="cst-check d-flex align-items-center">
-                <input type="checkbox" name="forms_check" value="{{ $form->id }}">
-                {{-- <input type="hidden" name="apptid" id="apptid"> --}}
-                <span class="checkmark me-2"></span> {{ $form->title }}
+                <input type="checkbox" name="add_forms_check" value="{{ $form->id }}">
+                <span class="checkmark me-2" ></span> {{ $form->title }}
             </label>
         </li>
     @endforeach
