7f56e7f0aaa53949c2f3008c1e7044f5fd16cc86
denishitcc <102940538+denishitcc@users.noreply.github.com>
2024-06-23 16:06:51 +0530

    bug fixes

--- resources/views/calender/index.blade.php
+++ resources/views/calender/index.blade.php
@@ -2757,6 +2757,7 @@
 
 
         $(document).on('change', '.all_staffs', function(e) {
+            debugger;
             // Get the selected option value
             var selectedValue = $(this).val();
             
@@ -2809,7 +2810,7 @@
                                 </div>
                                 <div class="mb-3">
                                     <label class="cst-check d-flex align-items-center">
-                                        <input type="checkbox" value="" class="current_date_checked">
+                                        <input type="checkbox" value="" class="current_date_checked" checked>
                                         <span class="checkmark me-2"></span>Filter waitlist by current calendar date
                                     </label>
                                 </div>
