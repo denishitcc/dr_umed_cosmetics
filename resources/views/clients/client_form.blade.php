cb639b4126ecd2820ff6047cb22da8643b3fab21
Dharit Maniyar <dharit@devsinindia.com>
2024-06-23 19:18:31 +0530

    working on form sectipn edit client

---
+++ resources/views/clients/client_form.blade.php
@@ -0,0 +1,8 @@
+<tr class="pnl-content">
+    <td colspan="3">
+        <div class="table-card-header">
+            <h5 id="formTitle"></h5> <button type="button" class="btn-close"></button>
+        </div>
+        <div id="fb-editor"></div>
+    </td>
+</tr>
